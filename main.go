db0f5bbc247eb2b29300b7f69eb0c9fce404943c
Aldiandya Irsyad <dragonopy@gmail.com>
2023-04-20 16:32:29 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/aldiandyaIrsyad/c3c2/handlers"
+)
+
+func main() {
+	router := gin.Default()
+
+	api := router.Group("/api")
+	{
+		api.POST("/register", handlers.Register)
+		api.POST("/login", handlers.Login)
+
+		authorized := api.Group("/", handlers.JWTAuthMiddleware())
+		{
+			authorized.POST("/products", handlers.CreateProduct)
+			authorized.GET("/products", handlers.GetProducts)
+			authorized.PUT("/products/:id", handlers.UpdateProduct)
+			authorized.DELETE("/products/:id", handlers.DeleteProduct)
+		}
+	}
+
+	router.Run(":8080")
+}
